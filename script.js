876d967fcb72314ac01b79045801c16d1f4b094d
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-01-12 12:11:00 -0300

    5 - metade do requisito feito

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
 /* BOTÕES */
-BOTÃO DE 
+//BOTÃO DE ADICIONAR TAREFAS
+//pegar meu botão
+const myButtonAdd = document.getElementById('criar-tarefa');
+//agr eu chamo o evento?
+myButtonAdd.addEventListener('click', teste)
+
+function teste() {
+    console.log("funfou")
+}
+
+
+
+// criar-tarefa.addEventListener(click, addTarefasAoClicar)
+
+//     function addTarefasAoClicar () {
+// //pegando o elemento..o valor dele.. a ideia é add ele na lista de tarefas quando clicar?????
+//    /*  const addTarefas = document.querySelector("#criar-tarefa").value
+        
+//     addTarefas.createElement('li') */
+    
+//     console.log("funcionou")
+// }   
+// /* 
+//     function cliqueDuplo () {
+       
+//    } */
+
+/* BOTÃO DE APAGAR TUDO 
+
+BOTÃO DE SUBIR 
+
+BOTÃO DE DESCER 
+
+BOTÃO DE LIMPAR OS COMPLETOS 
+
+BOTÃO DE LIMPAR LISTA 
+
+BOTÃO DE SALVAR LISTA  */
 
