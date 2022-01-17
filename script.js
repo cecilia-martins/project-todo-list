53768a5ef0f2721567dc3086756cfeeafae1a14c
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-01-17 17:51:27 -0300

    commitando

--- script.js
+++ script.js
@@ -1,40 +1,19 @@
 /* BOTÕES */
-//BOTÃO DE ADICIONAR TAREFAS
-//pegar meu botão
+/* //BOTÃO DE ADICIONAR TAREFAS
+//pegar meu botão */
 const myButtonAdd = document.getElementById('criar-tarefa');
-//agr eu chamo o evento?
+/* //agr eu chamo o evento? */
 myButtonAdd.addEventListener('click', teste)
-
+/* //minha função tem q pegar o valor digitado e add no final de uma lista???? */
 function teste() {
-    console.log("funfou")
+/* /////primeiro criando a li? */
+  const li = document.createElement('li')
+  /* ////pegando valor? */
+  const valorDigi = document.getElementById("criar-tarefa").value;
+  /* ////add esse valor a li? */
+    li.appendChild(valorDigi);
+
+  console.log("funfou");
 }
 
-
-
-// criar-tarefa.addEventListener(click, addTarefasAoClicar)
-
-//     function addTarefasAoClicar () {
-// //pegando o elemento..o valor dele.. a ideia é add ele na lista de tarefas quando clicar?????
-//    /*  const addTarefas = document.querySelector("#criar-tarefa").value
-        
-//     addTarefas.createElement('li') */
-    
-//     console.log("funcionou")
-// }   
-// /* 
-//     function cliqueDuplo () {
-       
-//    } */
-
-/* BOTÃO DE APAGAR TUDO 
-
-BOTÃO DE SUBIR 
-
-BOTÃO DE DESCER 
-
-BOTÃO DE LIMPAR OS COMPLETOS 
-
-BOTÃO DE LIMPAR LISTA 
-
-BOTÃO DE SALVAR LISTA  */
-
+/* // criar-tarefa.addEventListener(click, addTarefasAoClicar) */
