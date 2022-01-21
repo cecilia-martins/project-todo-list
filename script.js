8073a3523d883270a1c9760c13855096099fa2af
Cecília Martins <cecilia_lmartins@hotmail.com>
2022-01-21 10:36:40 -0300

    pegando o resto dos botões

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+// BOTÕES
 // pegando meu texto-tarefa.. input onde escreve a tarefa
 let textoTarefa = document.getElementById('texto-tarefa');
 // pegando meu criar-tarefa.. botao que add tarefas
 let btnCriarTarefas = document.querySelector('#criar-tarefa');
 // pegando minha lista
 let lisDeTarefas = document.getElementById('lista-tarefas');
+// pegando o botão apaga-tudo
+let x = document.getElementById('apaga-tudo');
+// pegando o botão mover-cima
+let praCima = document.getElementById('mover-cima');
+// pegando o botão mover-baixo
+let praBaixo = document.getElementById('mover-baixo');
+// pegando o botão remover-finalizados
+let removeFinalizado = document.getElementById('remover-finalizados');
+// pegando o botão remover-selecionado
+let removeSelecionado = document.getElementById('remover-selecionado');
+// pegando o  botão salvar-tarefas
+let salvar = document.getElementById('salvar-tarefas');
 
 // criando uma função para add tarefas no trem
 function addTarefa() {
@@ -15,12 +28,8 @@ function addTarefa() {
 
   document.getElementById('texto-tarefa').value = ''; // redefinindo valor, pra poder limpar a caixinha
 }
-// FUNÇÃO PRA MUDAR A COR AO CLICAR AAAAAAAAAAAAAAAAAAAAAAH
-function mudarCor() {
-
-}
-
 // add o tal do evento de click
 btnCriarTarefas.addEventListener('click', addTarefa);
 // ENTENDI ERRADO??? NÃO SEI, MAS TA FUNCIONANDO KKKKKKK <33333
 // NÃO MEXE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
